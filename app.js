9a79930f97834f2eeca01a57ebd78f23a7e33a7a
grantfogle <grantjfogle@gmail.com>
2018-11-30 13:03:32 -0700

    added rest of CRUD commands

--- app.js
+++ app.js
@@ -7,4 +7,23 @@ app.get('/', (req, res) => {
     queries.listAll().then(drills => res.send(drills))
 })
 
+app.get('/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    queries.getById(id).then(drills => res.send(drills))
+})
+
+app.post('/', (req, res) => {
+    queries.createDrill(req.body).then(drills => res.send(drills[0]))
+})
+
+app.delete('/:id', (req, res) => {
+    queries.deleteDrill(req.params.id).then(drills => res.send({ status: 204, message: "it has been deleted" }))
+})
+
+app.put('/:id', (req, res) => {
+    queries.updateDrill(req.params.id, req.body).then(updatedDrill => res.json(updatedDrill[0]));
+})
+
+
 app.listen(port, console.log(`listening on ${port}!`));
--- queries.js
+++ queries.js
@@ -3,5 +3,21 @@ const database = require('./database-connection')
 module.exports = {
     listAll() {
         return database('drills')
+    },
+    getById(id) {
+        return database('drills').where('id', id)
+    },
+    createDrill(newDrill) {
+        return database('drills').insert(newDrill).returning('*')
+    },
+    deleteDrill(id) {
+        return database('drills')
+            .where('id', id)
+            .delete();
+    },
+    updateDrill(id, drill) {
+        return database('drills')
+            .where('id', id)
+            .update(drill)
     }
 }
