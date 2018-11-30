9a68055f4524f010215f13f64151c58537e0376f
grantfogle <grantjfogle@gmail.com>
2018-11-30 11:18:33 -0700

    created migration folder, exports up and exports, set up first table

---
+++ migrations/20181130105518_drills.js
@@ -0,0 +1,16 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('drills', (drill) => {
+        drill.increments('id')
+        drill.string('name')
+        drill.string('skill_level')
+        drill.string('type')
+        drill.boolean('solo')
+        drill.integer('reps')
+        drill.string('description')
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('drills');
+};
