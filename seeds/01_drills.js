ba81d5e21cc98fa2b6b77731564390c9a5656c2a
grantfogle <grantjfogle@gmail.com>
2018-11-30 11:34:25 -0700

    added seed data

---
+++ seeds/01_drills.js
@@ -0,0 +1,82 @@
+
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('drills').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('drills').insert([
+        {
+          name: 'Triangle Choke',
+          skill_level: 'White',
+          type: 'Submission',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Armbar',
+          skill_level: 'White',
+          type: 'Submission',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Rear Naked Choke',
+          skill_level: 'White',
+          type: 'Submission',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Americana',
+          skill_level: 'White',
+          type: 'Submission',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Kimura',
+          skill_level: 'White',
+          type: 'Submission',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Cross Collar',
+          skill_level: 'White',
+          type: 'Submission',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Arm Triangle',
+          skill_level: 'White',
+          type: 'Submission',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Pendulum Sweep',
+          skill_level: 'White',
+          type: 'Sweep/Escape',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          name: 'Side Control Escape',
+          skill_level: 'White',
+          type: 'Sweep/Escape',
+          solo: false,
+          reps: 10,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+      ]);
+    });
+};
